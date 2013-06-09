20eceb0660b6728d799d4bad2065800b600bee86
Till Hülsemann <thgithub@gmail.com>
2013-06-09 03:57:08 +0200

    add constructor with specific values for ork-class

--- ork.php
+++ ork.php
@@ -3,15 +3,25 @@ class Ork {
 	public final $maxAttribute = 20;
 	public final $maxStrength = maxAttribute;
 	public final $maxEndurance = maxAttribute;
-	public final $maxSpeed = maxAttribute;	
+	public final $maxSpeed = maxAttribute;
 
 	protected $strength;
 	protected $endurance;
 	protected $speed;
+
+	protected $engage = false;
+	
 	function __construct() {
 		$strength = rand(1, $maxStrength);
 		$endurance = rand(1, $maxEndurance);
 		$speed = rand(1, $maxSpeed);
 	}
+
+	function __construct($strength, $endurance, $speed) {
+		$this->strength = $strength;
+		$this->endurance = $endurance;
+		$this->speed = $speed;
+	}
+
 }
 ?>
