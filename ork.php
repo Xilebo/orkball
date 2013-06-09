178672ab979f5aa7cb8a0d3a12c1b3f49de87efc
Till Hülsemann <thgithub@gmail.com>
2013-06-09 03:44:04 +0200

    fix some syntax errors in ork.php

--- ork.php
+++ ork.php
@@ -1,17 +1,17 @@
 <?php
 class Ork {
-	public final maxAttribute = 20;
-	public final maxStrength = maxAttribute;
-	public final maxEndurance = maxAttribute;
-	public final maxSpeed = maxAttribute;	
+	public final $maxAttribute = 20;
+	public final $maxStrength = maxAttribute;
+	public final $maxEndurance = maxAttribute;
+	public final $maxSpeed = maxAttribute;	
 
 	protected $strength;
 	protected $endurance;
 	protected $speed;
-	function _function __construct() {
-		$strength = rand(1, maxStrength);
-		$endurance = rand(1, maxEndurance);
-		$speed = rand(1, maxSpeed);
+	function __construct() {
+		$strength = rand(1, $maxStrength);
+		$endurance = rand(1, $maxEndurance);
+		$speed = rand(1, $maxSpeed);
 	}
 }
 ?>
