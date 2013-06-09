b22aec89644fe8a957c2832bfa6e5d9eb57a94ea
Till Hülsemann <thgithub@gmail.com>
2013-06-09 05:50:55 +0200

    add basic team class
    currently it is just initialized with a given name and optionally with a given number of random orks

---
+++ team.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once 'ork.php';
+	
+class Team {
+
+	protected $name;
+	protected $orks = array();
+	
+	public function __construct($name, $initialSize = 0) {
+		$this->name = $name;
+		for ($i = 0; $i < $initialSize; $i++) {
+			$this->addOrk();
+		}
+	}
+	
+	public function addOrk($ork = FALSE) {
+		if ($ork) {
+			$this->orks[] = $ork;
+		} else {
+			$this->orks[] = new Ork(Ork::generateName(), $this->name);
+		}
+	}
+	
+}
+?>
