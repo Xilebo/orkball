cc6551d27d0033793aadb13c286f9eb7c620599b
Till Hülsemann <thgithub@gmail.com>
2013-06-09 04:16:49 +0200

    add basic fighting logic for ork

--- ork.php
+++ ork.php
@@ -1,16 +1,27 @@
 <?php
 class Ork {
+
+	//constants
+
 	public final $maxAttribute = 20;
 	public final $maxStrength = maxAttribute;
 	public final $maxEndurance = maxAttribute;
 	public final $maxSpeed = maxAttribute;
 
+	//variables
+
 	protected $strength;
 	protected $endurance;
 	protected $speed;
 
-	protected $engage = false;
-	
+	protected $maxHealth;
+	protected $damage;
+
+	protected $health;
+	protected $targetEnemy = false;
+
+	// initialize
+
 	function __construct() {
 		$strength = rand(1, $maxStrength);
 		$endurance = rand(1, $maxEndurance);
@@ -22,22 +33,46 @@ class Ork {
 		$this->endurance = $endurance;
 		$this->speed = $speed;
 	}
-	
+
+	protected function init() {
+		$this->health = $this->endurance * 10;
+		$this->damage = $this->strength;
+	}
+
 	//Getters and Setters
-	
+
 	public function getStrength() {
 		return $this->strength;
 	}
-	
+
 	public function getEndurance() {
 		return $this->endurance;
 	}
-	
+
 	public function getSpeed() {
 		return $this->speed;
 	}
-	
-	//logic
-	
+
+	//fighting
+
+	public function engage($enemy) {
+		$this->targetEnemy = $enemy;
+	}
+
+	public function hit($damage) {
+		$this->health -= $damage;
+
+		//TODO check if dead
+
+	}
+
+	public function attackTarget() {
+		if ($this->targetEnemy) {
+			$this->targetEnemy.hit($this->damage);
+
+			//TODO check if enemy dead
+		}
+	}
+
 }
 ?>
